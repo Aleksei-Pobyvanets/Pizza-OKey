5fda367b61045b2766786796444ebd6cff663a3e
Alex Pobyvanets <alekseipobyvanets@gmail.com>
2020-01-31 18:08:48 +0200

    something

--- pizza.js
+++ pizza.js
@@ -11,3 +11,17 @@ function openProduct(evt, productName) {
   document.getElementById(productName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+function openProduct(evt, productName) {
+  var i, tabcontent, tablinks__more;
+  tabcontent = document.getElementsByClassName("tabMorecontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks__more = document.getElementsByClassName("tablinks__more");
+  for (i = 0; i < tablinks__more.length; i++) {
+    tablinks__more[i].className = tablinks__more[i].className.replace(" active", "");
+  }
+  document.getElementById(productName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
