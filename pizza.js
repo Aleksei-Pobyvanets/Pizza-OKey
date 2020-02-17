7dbb8d07c71db2c9dc28713d6df45b54610c15c7
Alex Pobyvanets <alekseipobyvanets@gmail.com>
2020-02-17 20:15:23 +0200

    something

--- pizza.js
+++ pizza.js
@@ -12,38 +12,8 @@ function openProduct(evt, productName) {
   evt.currentTarget.className += " active";
 }
 
-// More
 
-function openProduct1(evt, productName) {
-  var i, tabcontent, tablinks__more;
-  tabcontent = document.getElementsByClassName("tabMorecontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks__more = document.getElementsByClassName("tablinks__more");
-  for (i = 0; i < tablinks__more.length; i++) {
-    tablinks__more[i].className = tablinks__more[i].className.replace(" active", "");
-  }
-  document.getElementById(productName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-
-//menu__moreProduct
-/*
-function moreProductMenu(menuStyle) {
-  var menuStyle = getComputedStyle(menu);
-  openMenu.onclock = function () {
-      if(menuStyle.display == "none"){
-          menu.classList.add("active");
-          this.innerHTML = "Закрыть"
-      }else {
-          menu.classList.remove("active");
-          this.innerHTML = "Открыть"
-      }
-  }
-}
-*/
+//More
 function moreProductMenuPizza() {
   document.getElementById("menuPizza").classList.toggle('active');
 }
